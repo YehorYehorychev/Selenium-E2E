7ecbf2bf5294fa9f13719ebde1379e65e2b99ba2
Yehor Yehorychev <egor57899@gmail.com>
2025-11-27 10:26:53 -0800

    First init test has been added

--- src/test/java/com/yehorychev/selenium/InitTest.java
+++ src/test/java/com/yehorychev/selenium/InitTest.java
@@ -1,16 +1,36 @@
 package com.yehorychev.selenium;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import java.util.Objects;
 
 public class InitTest {
-    public static void main(String[] args) throws InterruptedException {
 
-        WebDriver driver = new ChromeDriver();
-        driver.get("https://google.com");
-        Thread.sleep(2000);
+    @Test
+    void googleTest() {
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--disable-blink-features=AutomationControlled");
+        options.setExperimentalOption("excludeSwitches", new String[]{"enable-automation"});
+        options.setExperimentalOption("useAutomationExtension", false);
 
-        System.out.println(driver.getTitle());
+        WebDriver driver = new ChromeDriver(options);
+        driver.get("https://www.google.com/");
+
+        String pageTitle = driver.getTitle();
+        String pageUrl = Objects.requireNonNull(driver.getCurrentUrl()).split("\\?")[0];
+
+        System.out.printf("The page title is: %s, and the page base URL is: %s", pageTitle, pageUrl);
+
+        driver.findElement(By.xpath("//textarea[@class='gLFyf']")).click();
+        driver.findElement(By.xpath("//textarea[@class='gLFyf']")).sendKeys("Selenium");
+        driver.findElement(By.xpath("//textarea[@class='gLFyf']")).sendKeys(Keys.ENTER);
+        Assert.assertTrue(driver.findElement(By.xpath("(//span[contains(text(),'Selenium Test Automation')])[1]")).isDisplayed());
 
         driver.quit();
     }
