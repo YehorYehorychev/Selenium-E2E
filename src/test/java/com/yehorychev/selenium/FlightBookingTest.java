9a497784d447d6797ca5ebc25e87000b9e594196
Yehor Yehorychev <egor57899@gmail.com>
2025-11-28 09:44:41 -0800

    FlightBooking test has been refactored

--- src/test/java/com/yehorychev/selenium/FlightBookingTest.java
+++ src/test/java/com/yehorychev/selenium/FlightBookingTest.java
@@ -1,9 +1,13 @@
 package com.yehorychev.selenium;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.support.ui.Select;
 import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 import java.time.Duration;
@@ -25,6 +29,14 @@ public class FlightBookingTest {
 
         String pageTitle = driver.getTitle();
         String pageUrl = Objects.requireNonNull(driver.getCurrentUrl()).split("\\?")[0];
+
+        System.out.printf("The page title is: %s, and the page base URL is: %s", pageTitle, pageUrl);
+
+        WebElement staticDropdown = driver.findElement(By.id("ctl00_mainContent_DropDownListCurrency"));
+        Select dropdown = new Select(staticDropdown);
+
+        dropdown.selectByIndex(3);
+        Assert.assertEquals(dropdown.getFirstSelectedOption().getText(), "USD");
 
         driver.quit();
     }
